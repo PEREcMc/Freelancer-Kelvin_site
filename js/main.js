1a912395e10986874db761a6cac4e2e2ed394d87
PEREcMC <perecvlasenko@gmail.com>
2022-07-26 19:28:14 +0300

    created project

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const nav = document.getElementById('nav');
+const navBtn = document.getElementById('nav-btn');
+const navBtnImg = document.getElementById('nav-btn-img');
+
+
+navBtn.onclick = () => {
+
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./img/icons/nav-close.svg";
+    } else {
+        navBtnImg.src = "./img/icons/nav-open.svg";
+    }
+};
+
+AOS.init();
+
+// AOS.init({
+
+//     disable: 'phone'
+//     once: true
+// }
+// );
